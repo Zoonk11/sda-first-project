8810a7009afbe5143161dbf7e91a01a76928c7a5
Damian Lewandowski <a@b.pl>
2020-08-23 12:33:20 +0200

    First comitt with initial project

---
+++ src/com/company/Hero.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Hero {
+    public static int heroesCount = 0; // bez static kazdy ma swoja zmienna
+    private String name = "Goliath";
+    private char sex = 'M';
+    private int health = 100;
+    private int strength = 10;
+    public Hero(){
+//        System.out.println("przed " + heroesCount++);
+//        heroesCount++;
+//        System.out.println("po " + heroesCount++);
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void printInfo() {
+        if(heroesCount==1000){          //bezpieczniej wieksze rowno albo mniejsze rowne
+            System.out.println("cokolwiek");
+        }
+        System.out.println(name + " is " + sex + " and has " + health + " health and his strength is " + strength);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero();
+        Hero hero2 = new Hero();
+        hero1.setName("Gandalf");
+        hero2.setName("Frodo");
+        hero1.printInfo();
+        hero2.printInfo();
+    }
+}
